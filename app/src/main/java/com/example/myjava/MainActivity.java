8d9defb72f7419deced9c6dcde252d0bd3def322
FreeFlyXiaoMa <2429825530@qq.com>
2019-06-10 16:34:04 +0800

    单链表操作

--- app/src/main/java/com/example/myjava/MainActivity.java
+++ app/src/main/java/com/example/myjava/MainActivity.java
@@ -1,10 +1,39 @@
 package com.example.myjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-class Node<E>{
+class Node<E> {
     E item;
+    Node<E> next;
+    //构造函数
+    Node(E element){
+        this.item=element;
+        this.next=null;
+    }
 
+
+
+
+
+
+    public void main(String[] args){
+        //头节点、尾节点
+        Node<E> head=null;
+        Node<E> tail=null;
+        //创建一个新的节点 并让head指向此节点
+        head=new Node("nodedata1");
+
+        //让尾节点指向此节点
+        tail=head;
+
+        //创建新节点 同时和最后一个节点连接起来
+        tail.next=new Node("node1data2");
+
+        //尾节点指向新的节点
+        tail=tail.next;
+
+    }
 
 }
